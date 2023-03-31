e7165014c766f0b86665d7fe34362523b8197a21
KafaBihi02 <124784453+KafaBihi02@users.noreply.github.com>
2023-04-01 01:56:17 +0700

    membuat method setor

--- src/objectATM.java
+++ src/objectATM.java
@@ -2,5 +2,18 @@ import java.util.Scanner;
 
 public class objectATM {
     public static int id_uang;
+    public void setor() {
+//method non return tanpa parameter
+Scanner input = new Scanner(System.in);
+Integer setor = input.nextInt();
+if (setor >= 20000) {
+ATM.saldo = ATM.saldo + setor;
+System.out.println("Saldo anda sekarang Rp" + ATM.saldo);
+ATM.kembali = true;
+} else {
+System.out.println("Masukkan nominal minimal Rp20.000");
+setor();
+}
+}
 
 }
