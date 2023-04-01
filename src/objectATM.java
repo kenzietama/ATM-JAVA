45ac087d083be20624edfd1d1f055629ce91baa6
Aura Narendra <narendra.maheswara0908@gmail.com>
2023-04-01 14:30:08 +0700

    Menambah fungsi tarik tunai.

--- src/objectATM.java
+++ src/objectATM.java
@@ -15,5 +15,20 @@ System.out.println("Masukkan nominal minimal Rp20.000");
 setor();
 }
 }
+    public void tariktunai() {
+        Scanner masuk = new Scanner(System.in);
+        //jumlah saldo nasabah
+        System.out.println("Saldo : Rp" + ATM.saldo);
+
+        //pecahan mata uang tersedia
+        System.out.println("Pecahan uang yang tersedia :");
+        for (int id = 0; id <= 2; id++) {
+            System.out.println(id + 1 + ". " + "Rp" + ATM.nominal[id]);
+        }
+
+        //pilih pecahan uang
+        System.out.println("Gunakan angka (1-3) untuk memilih denominasi uang, 0 untuk membatalkan");
+        id_uang = masuk.nextInt();
+    }
 
 }
